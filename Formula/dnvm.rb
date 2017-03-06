63e7bb39af5f1a611e15e5a4969eeb13e8b98616
Nate McMaster <nate.mcmaster@gmail.com>
2017-03-05 19:45:43 -0800

    Cleanup formula and update to latest build

--- Formula/dnvm.rb
+++ Formula/dnvm.rb
@@ -1,21 +1,30 @@
 class Dnvm < Formula
-  desc ".NET Core Environment Manager"
+  desc ".NET Core Version Manager"
   homepage "https://github.com/natemcmaster/dnvm"
-  devel do
-    url "https://dl.dropboxusercontent.com/u/72771334/dnvm.osx-x64.0.1.0-alpha.tar.gz"
-    version "0.1.0-alpha"
-    sha256 "fa3e46e19a325e82b187f33d374fcc71ee2b797ea680d774917a3c362eeb6b1c"
-  end
+  url "https://dnvmtools.blob.core.windows.net/cli/0.1.0/dnvm.osx-64.tar.gz"
+  version "0.1.0"
+  sha256 "d34961c1e2d10479063835d6e43d4ce9d5bdacb598146127670953cbc3a25ccd"
 
   depends_on "openssl"
+  depends_on "libyaml"
+
+  option "without-dotnet", "Install DNVM without install the 'dotnet' executable"
+  option "without-sdk", "Install DNVM without installing the latest .NET Core SDK"
 
   def install
-    system "cp", "-R", ".", prefix
-    bin.install bin/"dotnet"
+    cp_r ".", prefix
+
+    bin.install bin/"dotnet" if build.with?("dotnet")
     bin.install bin/"dnvm"
+    bin.install bin/"dnvm-prompt"
+
+    libexec.install_symlink "#{HOMEBREW_PREFIX}/opt/openssl/lib/libcrypto.1.0.0.dylib"
+    libexec.install_symlink "#{HOMEBREW_PREFIX}/opt/openssl/lib/libssl.1.0.0.dylib"
+    ohai 'Installing the latest .NET Core. This may take a few minutes...' 
+    system bin/"dnvm", "--verbose", "install", "sdk", "stable" if build.with?("sdk")
   end
 
   test do
-    assert shell_output("dnvm --version", result=2).include?("0.1.0-alpha"), "dnvm --version didn't include expected version'"
+    assert shell_output("#{bin}/dnvm install sdk stable", result=0)
   end
 end
